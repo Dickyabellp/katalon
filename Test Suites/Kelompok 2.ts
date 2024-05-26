7c6265c67fe167a4253dc82822bf4c40856800e9
RICKY-HARBU <RICKY-HARBU@DESKTOP-HARBU>
2024-05-26 21:47:49 +0700

    bismillah

---
+++ Test Suites/Kelompok 2.ts
@@ -0,0 +1,70 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Kelompok 2</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>7</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>01e18d3c-5cee-4c91-9f53-abed5fb80aa4</testSuiteGuid>
+   <testCaseLink>
+      <guid>729f1ab6-ae40-4b60-9981-7d6fbfdccaf2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Login_Normal</testCaseId>
+      <testDataLink>
+         <combinationType>ONE</combinationType>
+         <id>f70b4162-c922-4333-a363-886a29ec954d</id>
+         <iterationEntity>
+            <iterationType>ALL</iterationType>
+            <value></value>
+         </iterationEntity>
+         <testDataId>Data Files/LoginData</testDataId>
+      </testDataLink>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+      <variableLink>
+         <testDataLinkId>f70b4162-c922-4333-a363-886a29ec954d</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>username</value>
+         <variableId>528b91f4-6534-4fa0-bccd-b29e634f61fa</variableId>
+      </variableLink>
+      <variableLink>
+         <testDataLinkId>f70b4162-c922-4333-a363-886a29ec954d</testDataLinkId>
+         <type>DATA_COLUMN</type>
+         <value>password</value>
+         <variableId>25274c51-e66a-4c06-a085-08a2d2ddc1b8</variableId>
+      </variableLink>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4a65241c-cbe6-4c62-97b8-8ef7fe37f691</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RecordEditProfileAkun/EditProfileAkun</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>189604c0-ca14-4d01-8f9d-947cdbc7ef75</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RecordLoginLogout/LoginTestAbnormal</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f7d5dba1-23a0-4067-a771-ef4d17c01a40</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/RecordLoginLogout/LogoutTest</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>74e9cd00-0c4b-4975-a257-7c95b7924c17</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Web_MA/Menu_Program_manual</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
